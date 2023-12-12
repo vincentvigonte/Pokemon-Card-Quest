a805df0ca8ba6f9ad42373d45f9cae9dd9691b79
unknown <202280295@psu.palawan.edu.ph>
2023-12-12 13:47:49 +0800

    modify cardquest

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Trainer, PokemonCard
+from .models import Trainer, PokemonCard, Collection
 
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
@@ -11,3 +11,8 @@ class TrainerAdmin(admin.ModelAdmin):
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ('name', 'rarity')
     search_fields = ('name',)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('trainer', 'card', 'created_at', 'updated_at')
+    search_fields = ('trainer', 'card')
