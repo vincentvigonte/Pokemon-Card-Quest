ae9a2f9626dd2900fa2ca6a5b9daa53006137d1f
unknown <202280295@psu.palawan.edu.ph>
2023-12-13 11:03:30 +0800

    show all fields

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -3,16 +3,15 @@ from .models import Trainer, PokemonCard, Collection
 
 @admin.register(Trainer)
 class TrainerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'birthdate', 'location', 'email')
-
+    list_display = ('name', 'birthdate', 'location', 'email', 'created_at', 'updated_at')
     search_fields = ('name', 'location')
 
 @admin.register(PokemonCard)
 class PokemonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rarity')
+    list_display = ('name', 'rarity', 'hp', 'card_type', 'attack', 'description', 'weakness', 'card_number', 'release_date', 'evolution_stage', 'abilities', 'created_at', 'updated_at')
     search_fields = ('name',)
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('trainer', 'card', 'created_at', 'updated_at')
+    list_display = ('trainer', 'card', 'collection_date', 'created_at', 'updated_at')
     search_fields = ('trainer', 'card')
